30a3b288a139367ae0ece98f6848e6e94b857a3a
Kevin Attinger <kevin.attinger@web.de>
2024-04-19 12:16:58 +0200

    setup shad ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`font-sans ${inter.variable}`}>
+        <body className={`font-sans ${inter.variable} dark`}>
           <BottomNav />
           {children}
           {modal}
